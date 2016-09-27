37c9a5f0070180f433f54e9b015db044eb13f036
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-09-26 18:22:23 -0700

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,21 @@
 'use strict';
+function addIngredient(recipe, ingredientName, amount){
+	recipe[ingredientName] = amount;
+	return recipe;
+}
+
+function removeIngredient(recipe, ingredientName){
+	delete recipe[ingredientName];
+	return recipe;
+}
+
+function updateIngredient(recipe, ingredientName, amount){
+	recipe[ingredientName] = amount;
+	return recipe;
+}
+
+function readRecipe(recipe){
+	for (var key in recipe){
+	console.log("this recipe calls for " + recipe[key] + " of " + key);
+	}
+}
